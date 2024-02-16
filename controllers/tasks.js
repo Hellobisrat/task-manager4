99470a990b113d63fd42a2524e57cfe088c9c803
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2024-02-16 12:17:03 -0600

    all CRUD operation applied on the controllerfile

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,23 +1,61 @@
 const Task = require('../models/Task.js')
 
-const getAllTasks =(req,res)=>{
-  res.send('all items')
+const getAllTasks = async(req,res)=>{
+  try {
+    const tasks = await Task.find({})
+  res.status(201).json({tasks:tasks})
+  } catch (error) {
+    res.status(500).json({msg:error})
+  }
 }
 
 const createTask = async(req,res)=>{
-  const task = await Task.create(req.body)
+  try {
+    const task = await Task.create(req.body)
   res.status(201).json({task})
+  } catch (error) {
+    res.status(500).json({msg:error})
+  }
+  
 }
-const getTask = (req,res)=>{
-  res.send('get a single task')
+const getTask = async(req,res)=>{
+  try {
+    const{id:taskId}=req.params
+    const task = await Task.findOne({_id:taskId})
+    if(!task){
+      return res.status(404).json({msg:`No task with id:${taskId}`})
+    }
+  res.status(201).json({task})
+  } catch (error) {
+    res.status(500).json({msg:error})
+  }
 }
 
-const updateTask = (req,res)=>{
-  res.send('update task')
+const updateTask = async(req,res)=>{
+  try {
+    const{id:taskId}=req.params
+    const task = await Task.findOneAndUpdate({_id:taskId},req.body,{new:true,runValidators:true})
+    if(!task){
+      return res.status(404).json({msg:`No task with id:${taskId}`})
+    }
+  res.status(201).json(task)
+  } catch (error) {
+    res.status(500).json({msg:error})
+  }
 }
 
-const deleteTask = (req,res)=>{
-  res.send('delete task')
+
+const deleteTask = async (req,res)=>{
+  try {
+    const{id:taskId}=req.params
+    const task = await Task.findOneAndDelete({_id:taskId})
+    if(!task){
+      return res.status(404).json({msg:`No task with id:${taskId}`})
+    }
+  res.status(201).json({task})
+  } catch (error) {
+    res.status(500).json({msg:error})
+  }
 }
 
 
